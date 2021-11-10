69d5dcdd63127b3a85e9d9c79b5bc9a82eaa9dcb
Jc <jean.christophe.molhant@gmail.com>
2021-11-10 15:32:58 +0100

    added responsiveness to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,21 +6,23 @@ function HomePage({state}) {
         <React.Fragment>
             <TodayCard state={state} />
             <h3>Upcoming</h3>
-            {state.upcoming.map(x => {
-                return (
-                    <div className="upcoming card" key={x.id}>
-                        <div className="upcoming__data">
-                            <h4>{x.task}</h4>
-                            <p>{x.status ? 'Déjà fait' : 'Encore à faire'}</p>
-                            <p>{x.date}</p>
+            <div className="upcomingContainer">
+                {state.upcoming.map(x => {
+                    return (
+                        <div className="upcoming card" key={x.id}>
+                            <div className="upcoming__data">
+                                <h4>{x.task}</h4>
+                                <p>{x.status ? 'Déjà fait' : 'Encore à faire'}</p>
+                                <p>{x.date}</p>
+                            </div>
+                            <div className="upcoming__actions">
+                                <p>O</p>
+                                <p>V</p>
+                            </div>
                         </div>
-                        <div className="upcoming__actions">
-                            <p>O</p>
-                            <p>V</p>
-                        </div>
-                    </div>
-                );
-            })}
+                    );
+                })}
+            </div>
         </React.Fragment>
     );
 }
